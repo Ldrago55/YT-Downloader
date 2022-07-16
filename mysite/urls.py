1e4ce7ca6e9e5e5f50b8b142530dfb80b83fa7f2
Ldrago55 <bjana2580@gmail.com>
2022-07-16 21:51:28 +0530

    commit changed

--- mysite/urls.py
+++ mysite/urls.py
@@ -19,12 +19,11 @@ from django.urls import path, include
 
 
 from django.views.static import serve
-from django.conf.urls import re_path as url
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('calc.urls')),
-    url(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}), 
-    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
+    
     
 ]
