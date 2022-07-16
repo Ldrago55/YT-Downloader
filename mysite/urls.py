11775133af92039de8f540eb3996ecd8321bc12a
Ldrago55 <bjana2580@gmail.com>
2022-07-16 21:17:41 +0530

    commit changes

--- mysite/urls.py
+++ mysite/urls.py
@@ -19,7 +19,7 @@ from django.urls import path, include
 
 
 from django.views.static import serve
-from django.conf.urls import url
+from django.conf.urls import re_path as url
 
 urlpatterns = [
     path('admin/', admin.site.urls),
