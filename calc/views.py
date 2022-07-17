b9a1522b89310de872e0375924888e204936c2d0
Ldrago55 <bjana2580@gmail.com>
2022-07-17 08:40:36 +0530

    commit changes

--- calc/views.py
+++ calc/views.py
@@ -8,17 +8,9 @@ def index(request):
 
 def search(request):
     if request.method == 'POST':
-        import requests
-        link = request.POST.get('link')
-        clg_name = link.replace(" ", "+")
-        USER_AGENT = "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/44.0.2403.157 Safari/537.36"
-        LANGUAGE = "en-US,en;q=0.5"
-        session = requests.Session()
-        session.headers['User-Agent'] = USER_AGENT
-        session.headers['Accept-Language'] = LANGUAGE
-        session.headers['Content-Language'] = LANGUAGE
-        html_content = session.get(f'https://www.google.com/search?q=+{clg_name}').text
-        return html_content
+        link = request.POST['link']
+        search_result = webbrowser.open("https://www.google.com/search?q={}".format(link))        
+        return render(request,"search.html")
 
         
 
